dba86d8351a604b21d7ed7355135a4678051a891
gurpreetSingh4 <gurpreetyarasingh@gmail.com>
2024-09-28 00:06:29 +0530

    build homePage

---
+++ backend/src/index.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+    .then(()=>console.log("mongoDB connected"))
+    .catch((err)=> console.log("err"));
+
+const PORT = 3000;
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+app.get("/test", async (req: Request, res: Response) => {
+res.json({
+message: "Hello!"
+});
+});
+
+app.listen(PORT, () => {
+console.log(`Server started on localhost:${PORT}`);
+});
+   
