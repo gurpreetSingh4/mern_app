d9a0aabcbae1a6490135e03dfb1a2e696fa72ab5
gurpreetSingh4 <gurpreetyarasingh@gmail.com>
2024-09-28 18:55:05 +0530

    home page setup and create route using express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,24 +2,19 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import MyUserRoute from "./routes/MyUserRoute"
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
     .then(()=>console.log("mongoDB connected"))
     .catch((err)=> console.log("err"));
 
-const PORT = 3000;
+const PORT = 7000;
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-
-app.get("/test", async (req: Request, res: Response) => {
-res.json({
-message: "Hello!"
-});
-});
+app.use("/api/my/user", MyUserRoute);
 
 app.listen(PORT, () => {
 console.log(`Server started on localhost:${PORT}`);
 });
-   
