d43adbe439ea49a1dcf1cda7ef19d0c4a473ec6c
gurpreetSingh4 <gurpreetyarasingh@gmail.com>
2024-09-29 19:31:48 +0530

    update user profile request

--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -35,5 +35,37 @@ export const useCreateMyUser = () => {
         isLoading,
         isError,
         isSuccess
-    }
+    }; 
+}
+
+type UpdateMyUserRequest = {
+    name: string;
+    addressLine1: string;
+    city: string;
+    country: string;
+
+}
+export const useUpdateMyUser = () => {
+    const { getAccessTokenSilently} = useAuth0();
+
+    const updateMyUserRequest = async (formData :UpdateMyUserRequest) => {
+        const accessToken = await getAccessTokenSilently();
+        const response = await fetch(`${API_BASE_URL}/api/my/user` , {
+            method: "PUT",
+            headers: {
+                Authorization: `Bearer ${accesstoken}`,
+                "Content-Type": "application/json", 
+            },
+            body: JSON.stringify(formData),
+        });
+        if(!response.ok){
+            throw new Error("Failed to update user")
+        }
+
+        return response.json();
+    };
+
+    const { mutateAsync: updateUser, isLoading, isSuccess, isError, error, reset,} = useMutation(updateMyUserRequest);
+
+    return {updateUser, isLoading};
 }
